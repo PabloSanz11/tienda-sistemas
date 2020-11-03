5895de9b3a06cdba70a1f2269803fe8de1309e6b
PabloSanz11 <72416957+PabloSanz11@users.noreply.github.com>
2020-11-03 16:01:59 -0600

    Guardar dentro de htdocs

---
+++ php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+	function conectate(){
+		$con = mysqli_connect('us-cdbr-east-02.cleardb.com','b5bb920c4d74d4','7b5379b9','heroku_2eb4075a39b1458');
+		return $con;
+	}
+
+	function consulta($query){
+		$conexion  = conectate();
+		$res = mysqli_query($conexion,$query);
+		mysqli_close($conexion);
+		return $res;
+	}
+?>
